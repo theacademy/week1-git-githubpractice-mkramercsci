819907fc945e24378e9c0e20d7cb7e092fedd2fe
Michaela <mkramer.csci@gmail.com>
2023-05-09 10:01:58 -0600

    day two code along.

---
+++ README.md
@@ -0,0 +1,2 @@
+# week1-git-githubpractice-mkramercsci
+week1-git-githubpractice-mkramercsci created by GitHub Classroom
---
+++ day two/Michaela.java
@@ -0,0 +1,5 @@
+public class Michaela {
+    public static void main (String[] args) {
+        System.out.println("Hello, DI003 students! Hello professor. My name is Michaela and it is day two.");
+    }
+}
